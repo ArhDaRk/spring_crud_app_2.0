350fb868c4537768625fb57cbaf26f6a9a105d7f
Daniil Rubak <arhdark@yandex.ru>
2023-06-09 16:40:36 +0300

    ref service

--- src/main/java/web/repository/UserRepository.java
+++ src/main/java/web/repository/UserRepository.java
@@ -6,14 +6,14 @@ import web.model.User;
 import java.util.List;
 
 public interface UserRepository {
-    @Transactional
+
     void saveUser(User user);
-    @Transactional
+
     void updateUser(User user);
-    @Transactional
+
     void removeUserById(Integer id);
-    @Transactional(readOnly = true)
+
     List<User> getAllUsers();
-    @Transactional(readOnly = true)
+
     User getUserById(Integer id);
 }
--- src/main/java/web/service/UserServiceImp.java
+++ src/main/java/web/service/UserServiceImp.java
@@ -2,6 +2,7 @@ package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import web.model.User;
 import web.repository.UserRepository;
 
@@ -17,27 +18,27 @@ public class UserServiceImp implements web.service.UserService {
     public UserServiceImp(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-
+    @Transactional
     @Override
     public void saveUser(User user) {
         userRepository.saveUser(user);
     }
-
+    @Transactional
     @Override
     public void updateUser(User user) {
         userRepository.updateUser(user);
     }
-
+    @Transactional
     @Override
     public void removeUserById(Integer id) {
         userRepository.removeUserById(id);
     }
-
+    @Transactional(readOnly = true)
     @Override
     public List<User> getAllUsers() {
         return userRepository.getAllUsers();
     }
-
+    @Transactional(readOnly = true)
     @Override
     public User getUserById(Integer id) {
         return userRepository.getUserById(id);
